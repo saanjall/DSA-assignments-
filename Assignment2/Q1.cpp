caca11c1aa790e608c68e5b18863b3d2bc379b18
Saanjal Jain <sjain2_be24@thapar.edu>
2025-08-13 00:40:33 +0530

    Q1.cpp

---
+++ Assignment2/Q1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int binarySearch(int arr[ ], int size, int val) {
+int l = 0;
+int h = size - 1;
+
+while (l <= h) {
+int m = l + (h - l) / 2;
+
+if (arr[m] == val) {
+return m;
+}
+else if (arr[m] < val) {
+l = m + 1;
+}
+else {
+h = m - 1;
+}
+}
+
+return -1;
+}
+
+int main() {
+int arr[ ] = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72, 91};
+int size = sizeof(arr) / sizeof(arr[0]);
+int val;
+cin >> val;
+int result = binarySearch(arr, size, val);
+if (result != -1) {
+cout << "Element found at index " << result << endl;
+} else {
+cout << "Element not found in array." << endl;
+}
+return 0;
+}
